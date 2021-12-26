2fc4fd77232704e39d86faebd0e4d60cd4885164
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2021-12-26 20:09:06 +0530

    Added middleware for authentication and set up users endpoints

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,13 +1,72 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth")
+const { check, validationResult } = require('express-validator');
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 
+const User = require("../../models/User");
 
 // @route  GET api/auth
-// @desc   test
+// @desc   Get details of Usser
+// @access private
+
+router.get("/", auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({msg: "Server Error"})
+    }
+   
+})
+
+// @route  POST api/auth
+// @desc   Login and Generate token
 // @access public
 
-router.get("/", (req, res) => {
-    res.send("Auth route")
+router.post("/", [
+    check('email', "Please enter a valid email").isEmail(),
+    check('password',"Password is required ").exists()
+    
+] ,async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email })
+    
+        if (!user) {
+            return res.status(400).json({errors:[{msg:"Invalid Credentials"}]})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({errors:[{msg:"Invalid Credentials"}]})
+        }
+
+        const payLoad = {
+            user: {
+                id:user.id
+            }
+        }
+        
+        jwt.sign(payLoad, config.get("jwtSecret"), { expiresIn: 360000 }, (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        });
+    
+    } catch (err) {
+        console.error(error.message);
+        res.status(500).send("Server error");
+        }
+
+  
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const connectDB = require("./config/db")
 const app = express();
 
+//connect Database
 connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 
 app.get("/", (req, res) => {
     res.send("API Running");
